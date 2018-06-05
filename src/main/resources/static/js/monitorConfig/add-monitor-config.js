c0b2786c874b9b0dc97fcb3e8edfca9078ab72a1
gy <gaoyu3558@163.com>
2018-06-05 22:25:05 +0800

    配置页部分html

--- src/main/resources/static/js/monitorConfig/add-monitor-config.js
+++ src/main/resources/static/js/monitorConfig/add-monitor-config.js
@@ -9,6 +9,28 @@ define(['jquery', 'vue','commonModule'], function ($, Vue,commonModule) {
                 el: '#monitorConfig',
                 data: {
                     templateName: "cc",
+                    snmpVersion:"snmpv2",
+                    dataObj:{
+                        quotaName:'',
+                        quotaDesc:''
+                    },
+                    alertLevel:[
+                        {text:'',value:'0'},//紧急
+                        {text:'',value:'1'},//重要
+                        {text:'',value:'2'},//次要
+                        {text:'',value:'3'},//警告
+                        {text:'',value:'4'}]//通知
+                },
+                filters: {
+                    filterName:function(name) {
+
+                        return "";
+
+                    },
+                    filterDesc:function(desc) {
+
+                        return "";
+                    }
                 },
                 mounted: function () {
                     console.log("add monitor display")
@@ -27,7 +49,19 @@ define(['jquery', 'vue','commonModule'], function ($, Vue,commonModule) {
 
                             }
                         })
-                    }
+                    },
+                    showMoreLine :function (event) {
+                        var e = event.target;
+                        //closet从当前元素开始向上查找，包括当前元素
+                        $(e).closest('.span-more-line').addClass("hidden");
+                        $(e).closest('.span-more-line').closest('div').next().removeClass('hidden');
+                    },
+                    showMoreCondition:function (event) {
+                        var e = event.target;
+                        $(e).addClass("hidden");
+                        $(e).closest('div').next().removeClass('hidden')
+                    },
+                    //close 按钮没有做
                 }
 
             })
