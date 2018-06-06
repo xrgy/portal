167fee5bfdf7c8f8fd0fd7ea1d4cbf7d2d1252e3
gy <gaoyu3558@163.com>
2018-06-06 16:44:21 +0800

    各级告警按钮添加事件

--- src/main/resources/static/js/monitorConfig/add-monitor-config.js
+++ src/main/resources/static/js/monitorConfig/add-monitor-config.js
@@ -2,34 +2,46 @@
  * Created by gy on 2018/3/24.
  */
 'use strict'
-define(['jquery', 'vue','commonModule'], function ($, Vue,commonModule) {
+define(['jquery', 'vue', 'commonModule'], function ($, Vue, commonModule) {
     var monitorConf = function () {
         if ($('#monitorConfig')[0]) {
             var monitorConfig = new Vue({
                 el: '#monitorConfig',
                 data: {
                     templateName: "cc",
-                    snmpVersion:"snmpv2",
-                    dataObj:{
-                        quotaName:'',
-                        quotaDesc:''
-                    },
-                    alertLevel:[
-                        {text:'',value:'0'},//紧急
-                        {text:'',value:'1'},//重要
-                        {text:'',value:'2'},//次要
-                        {text:'',value:'3'},//警告
-                        {text:'',value:'4'}]//通知
+                    snmpVersion: "snmpv2",
+                    dataObj: [{
+                        quotaName: 'm',
+                        quotaDesc: ''
+                        },
+                        {
+                            quotaName: 'n',
+                            quotaDesc: ''
+                        }],
+                    typeName: [{
+                        name: '1',
+                        quotaDesc: ''
+                        },
+                        {
+                            name: '2',
+                            quotaDesc: ''
+                        }],
+                    alertLevel: [
+                        {text: '', value: '0'},//紧急
+                        {text: '', value: '1'},//重要
+                        {text: '', value: '2'},//次要
+                        {text: '', value: '3'},//警告
+                        {text: '', value: '4'}]//通知
                 },
                 filters: {
-                    filterName:function(name) {
+                    filterName: function (name) {
 
-                        return "";
+                        return name;
 
                     },
-                    filterDesc:function(desc) {
+                    filterDesc: function (desc) {
 
-                        return "";
+                        return name;
                     }
                 },
                 mounted: function () {
@@ -37,11 +49,11 @@ define(['jquery', 'vue','commonModule'], function ($, Vue,commonModule) {
                 },
                 methods: {
                     initData: function () {
-                        var _self =this;
+                        var _self = this;
                         console.log(commonModule.i18n("monitorConfig.templateName"));
                         $.ajax({
-                            data:{"id":"111"},
-                            url:"/getJPAInfo",
+                            data: {"id": "111"},
+                            url: "/getJPAInfo",
                             success: function (data) {
                                 _self.templateName = data.name;
                             },
@@ -50,22 +62,36 @@ define(['jquery', 'vue','commonModule'], function ($, Vue,commonModule) {
                             }
                         })
                     },
-                    showMoreLine :function (event) {
+                    //显示二级告警
+                    showMoreLine: function (event) {
                         var e = event.target;
                         //closet从当前元素开始向上查找，包括当前元素
                         $(e).closest('.span-more-line').addClass("hidden");
                         $(e).closest('.span-more-line').closest('div').next().removeClass('hidden');
                     },
-                    showMoreCondition:function (event) {
+                    //显示更多条件
+                    showMoreCondition: function (event) {
                         var e = event.target;
                         $(e).addClass("hidden");
-                        $(e).closest('div').next().removeClass('hidden')
+                        $(e).parent().next().removeClass('hidden');
                     },
-                    //close 按钮没有做
+                    //close按钮
+                    closeCondition: function (event) {
+                        var e = event.target;
+                        if ($(e).parent().prev('div').length !== 0) {
+                            $(e).parent().addClass("hidden");
+                            $(e).parent().prev('div').children('.span-more-condition').removeClass('hidden');
+                        } else {
+                            $(e).parent().next().addClass('hidden');
+                            $(e).closest('div').children('.span-more-condition').removeClass('hidden');
+                            $(e).parent().parent().prev('div').children('.span-more-line').removeClass('hidden');
+                            $(e).parent().parent().addClass('hidden');
+                        }
+                    }
                 }
 
             })
-            $("#monitorConfig").on('show.bs.modal',function () {
+            $("#monitorConfig").on('show.bs.modal', function () {
                 monitorConfig.initData();
             })
         }
